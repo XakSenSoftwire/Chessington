a02c342adecdfc91eb03f3272d641e2af28d1bc4
Xak.Sen <Xak.Sen@softwire.com>
2024-09-26 15:16:49 +0100

    rooks possible moves

--- Chessington.GameEngine/Pieces/Rook.cs
+++ Chessington.GameEngine/Pieces/Rook.cs
@@ -10,7 +10,45 @@ namespace Chessington.GameEngine.Pieces
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+            List<int> rowDisplacement = new List<int> {};
+            List<int> colDisplacement = new List<int> {};
+            
+        
+            Square currSquare = board.FindPiece(this);
+
+            for (int rosPosition = 0; rosPosition < 8; rosPosition++)
+            {
+                if (rosPosition !=currSquare.Row)
+                {
+                    rowDisplacement.Add(rosPosition-currSquare.Row);
+                }
+            }
+
+            for (int colPosition = 0; colPosition < 8; colPosition++)
+            {
+                if (colPosition != currSquare.Col)
+                {
+                    colDisplacement.Add(colPosition-currSquare.Col);
+                }
+            }
+            
+            IEnumerable<Square> possibleSquares = new List<Square>();
+            
+            // for loop to generate possible moves based on row Displacements
+            for (int idx = 0; idx < rowDisplacement.Count; idx++)
+            {
+                ((List<Square>)possibleSquares).Add(Square.At(currSquare.Row + rowDisplacement[idx],
+                    currSquare.Col));
+            }
+            
+            // for loop to generate possible moves based on row Displacements
+            for (int idx = 0; idx < colDisplacement.Count; idx++)
+            {
+                ((List<Square>)possibleSquares).Add(Square.At(currSquare.Row,
+                    currSquare.Col + colDisplacement[idx]));
+            }
+
+            return possibleSquares;
         }
     }
 }
