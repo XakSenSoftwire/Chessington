6e45412c2112dd6d223664724df63129fff16f2d
Xak.Sen <Xak.Sen@softwire.com>
2024-09-26 15:01:02 +0100

    pawns can now two-step if not moved before

--- Chessington.GameEngine/Pieces/Pawn.cs
+++ Chessington.GameEngine/Pieces/Pawn.cs
@@ -7,19 +7,45 @@ namespace Chessington.GameEngine.Pieces
 {
     public class Pawn : Piece
     {
-        public Pawn(Player player) 
+        public Pawn(Player player)
             : base(player) { }
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            int playerRowDirection = (Player == Player.Black) ? 1 : -1; 
-            // no need for playerColDirection
-            Square currSquare = board.FindPiece(this);
-            IEnumerable<Square> possibleSquares = new List<Square>
+            List<int> rowDisplacement = new List<int> {};
+            // no need for colDisplacement as pawns cannot en-passant or takes
+            
+            if (this.Player == Player.Black)
             {
-               // pawns only have one move (assuming no en-passant or two-step or takes) 
-                Square.At(currSquare.Row + playerRowDirection, currSquare.Col),
-            };
+                // normal pawn movement logic
+                rowDisplacement.Add(1);
+                
+                // special two-step logic
+                if (this.HasMoved == false)
+                {
+                    rowDisplacement.Add(2);
+                }
+
+            }
+            else
+            {
+                rowDisplacement.Add(-1);
+
+                if (this.HasMoved == false)
+                {
+                    rowDisplacement.Add(-2);
+                }
+            }
+
+            Square currSquare = board.FindPiece(this);
+            IEnumerable<Square> possibleSquares = new List<Square>();
+    
+            // for loop to generate possible moves based on row Displacements
+            for (int idx = 0; idx < rowDisplacement.Count; idx++)
+            {
+                ((List<Square>)possibleSquares).Add(Square.At(currSquare.Row + rowDisplacement[idx], currSquare.Col));
+            }
+            
             return possibleSquares;
         }
     }
